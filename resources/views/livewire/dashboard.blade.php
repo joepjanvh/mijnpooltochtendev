17b6e09aa3dfc1932034172518f49ae32098f3af
hansvana <hansvanarken@gmail.com>
2024-10-15 22:14:51 +0200

    Hike dashboard beter toegankelijk gemaakt voor kleine schermen

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,67 +1,100 @@
-<div class="container mx-auto pt-4" wire:poll.5000ms>
+<div class="container mx-auto pt-4" x-data="{ enablePolling: false }" >
     <div class="bg-white md:rounded-lg shadow p-2 md:p-6">
         <!-- Titel van de pagina -->
         <h1 class="text-2xl font-semibold mb-6">Overzicht van de posten voor editie {{ $edition->year }}</h1>
 
         <!-- Tabel met hikes en posten -->
         <div class="overflow-x-auto">
-            <table class="min-w-full bg-white border border-gray-300 table-fixed">
-                <thead class="bg-gray-100">
-                    <tr class="flex">
-                        @foreach($hikes as $hike)
-                        <th class="py-2 px-4 text-left text-sm font-medium text-gray-900 flex-1">
-                            <a href="/post-manager/{{ $hike->id }}" class="hover:underline text-blue-600">
-                                {{ $hike->hike_letter }}-Hike
-                            </a>
 
-                            <!-- Bereken de vertraging voor deze specifieke hike -->
-                            @if(isset($totalDelayPerHike[$hike->hike_letter]) && $totalDelayPerHike[$hike->hike_letter] > 0)
-                            <div class="bg-red-500 text-white text-center p-2 rounded mb-4 w-32 mx-auto whitespace-nowrap overflow-hidden text-ellipsis">
-                                    + {{ round($totalDelayPerHike[$hike->hike_letter], 2) }} min
+            <div class="flex justify-end">
+                <button 
+                    @click="enablePolling = !enablePolling" 
+                    class="text-black px-4 py-2 mb-2 rounded-md md:inline-block hidden">
+                    <span>
+                        <i :class="enablePolling ? 'fa-regular fa-square-check' : 'fa-regular fa-square'"></i> Automatisch updaten
+                    </span>
+                </button>   
+            </div> 
+
+            <div class="flex flex-col md:flex-row flex-nowrap":wire:poll.5000ms="enablePolling ? true : false">        
+    
+                @foreach($hikes as $hike)
+                <div class="grow flex flex-col border border-gray-250 bg-gray-100" data-accordion="open">
+                    <div id="accordion-collapse-heading-{{ $hike->hike_letter }}-hike">
+                        <button type="button" class="flex items-center flex-row md:flex-col w-full py-4 md:py-2 px-2"  data-accordion-target="#accordion-collapse-body-{{ $hike->hike_letter }}-hike" aria-expanded="false" aria-controls="accordion-collapse-body-{{ $hike->hike_letter }}-hike">
+                            <div class="flex-1 text-left">
+                                <svg data-accordion-icon class="w-6 h-6 shrink-0 rotate-180 md:hidden" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06 0L10 10.94l3.71-3.73a.75.75 0 111.06 1.06l-4.24 4.24a.75.75 0 01-1.06 0L5.23 8.27a.75.75 0 010-1.06z" clip-rule="evenodd"></path>
+                                </svg>
                             </div>
+                            <h3 class="text-1xl font-extrabold px-2 flex-1 text-center">
+                                <span class="inline-block md:hidden">{{ $hike->hike_letter }}-Hike</span>
+                                <a href="/post-manager/{{ $hike->id }}" class="hover:underline text-black hidden md:inline-block">
+                                    {{ $hike->hike_letter }}-Hike
+                                </a>
+                            </h3>
+                            <div class="flex-1 text-right">
+                            @if(isset($totalDelayPerHike[$hike->hike_letter]) && $totalDelayPerHike[$hike->hike_letter] > 0)
+                                <span class="text-sm font-medium px-2.5 py-0.5 rounded-full bg-red-500 text-white">+ {{ round($totalDelayPerHike[$hike->hike_letter], 2) }}</span>                        
                             @else
-                                <div class="bg-green-500 text-white text-center p-2 rounded mb-4 w-32 mx-auto whitespace-nowrap overflow-hidden text-ellipsis">
-                                    Op tijd
-                                </div>
+                                <span class="inline-flex items-center justify-center w-6 h-6 text-sm font-semibold text-white bg-green-500 rounded-full dark:bg-gray-700 dark:text-gray-300">
+                                <svg class="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+                                </svg>
+                                <span class="sr-only">Op tijd</span>
+                                </span>
                             @endif
-                        </th>
-                        @endforeach
-                    </tr>
-                </thead>
-                <tbody>
-                    @php
-                        // Bepaal het maximum aantal posten voor alle hikes
-                        $maxRows = max(array_map(fn($posts) => $posts->count(), $posts));
-                    @endphp
+                            </div>                            
+                        </button>
+                    </div>
+                    
 
-                    @for($i = 0; $i < $maxRows; $i++)
-                        <tr class="flex">
-                            @foreach($hikes as $hike)
-                                @php
-                                    $post = $posts[$hike->id][$i] ?? null;
-                                    $backgroundColor = 'bg-white'; // Standaardkleur
 
-                                    if ($post) {
-                                        // Bepaal de kleur op basis van de status
-                                        if ($post->time_close) {
-                                            $backgroundColor = 'bg-green-500 text-white'; // Post is gesloten
-                                        } elseif ($post->time_open) {
-                                            $backgroundColor = 'bg-orange-500 text-white'; // Post is geopend maar niet gesloten
-                                        } elseif (\Carbon\Carbon::now()->greaterThan(\Carbon\Carbon::parse($post->planned_time))) {
-                                            $backgroundColor = 'bg-red-500 text-white'; // Post had al geopend moeten zijn
-                                        }
+                    <div class="hidden md:block flex flex-col items-stretch w-full" id="accordion-collapse-body-{{ $hike->hike_letter }}-hike"  aria-labelledby="accordion-collapse-heading-{{ $hike->hike_letter }}-hike">
+                        @php
+                            // Bepaal het maximum aantal posten voor alle hikes
+                            $maxRows = max(array_map(fn($posts) => $posts->count(), $posts));
+                        @endphp
+
+                        @for($i = 0; $i < $maxRows; $i++)
+                            @php
+                                $post = $posts[$hike->id][$i] ?? null;
+                                $backgroundColor = 'bg-white'; // Standaardkleur
+
+                                if ($post) {
+                                    // Bepaal de kleur op basis van de status
+                                    if ($post->time_close) {
+                                        $backgroundColor = 'bg-green-500 text-white'; // Post is gesloten
+                                    } elseif ($post->time_open) {
+                                        $backgroundColor = 'bg-orange-500 text-white'; // Post is geopend maar niet gesloten
+                                    } elseif (\Carbon\Carbon::now()->greaterThan(\Carbon\Carbon::parse($post->planned_time))) {
+                                        $backgroundColor = 'bg-red-500 text-white'; // Post had al geopend moeten zijn
                                     }
-                                @endphp
-                                <td class="py-1 px-4 {{ $backgroundColor }} text-center border flex-1">
-                                    <a href="/post/{{ $post->id ?? '' }}" class="text-black hover:underline">
-                                        {{ $post->post_number ?? '' }}
-                                    </a>
-                                </td>
-                            @endforeach
-                        </tr>
-                    @endfor
-                </tbody>
-            </table>
+                                    @endphp
+                                    <div class="{{ $backgroundColor }} text-center border-b first:border-t last:border-0 py-4 md:py-1">
+                                        <a href="/post/{{ $post->id ?? '' }}" class="text-black hover:underline">
+                                            {{ $post->post_number ?? '' }}
+                                        </a>
+                                    </div>     
+                                    @php                               
+                                } else {
+                                    @endphp
+                                    <div class="{{ $backgroundColor }} text-center border-b first:border-t last:border-0 py-4 md:py-1 hidden md:block">
+                                        &nbsp;
+                                    </div>     
+                                    @php                                     
+                                }
+                            @endphp
+                        @endfor
+                        <div class="bg-blue-500 text-white inline-block md:hidden text-center">
+                            <a href="/post-manager/{{ $hike->id }}" class="inline-block text-white py-4">
+                                {{ $hike->hike_letter }}-Hike postenbeheer
+                            </a>
+                        </div>
+                    </div>                    
+                </div>
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
